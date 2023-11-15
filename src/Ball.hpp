222fb1e21ed354408bfcbefb564fbb1bc5952d90
Oporu <70465701+Oporu@users.noreply.github.com>
2023-11-15 23:37:26 +0800

    getting ready for more than 1 trail per render

--- src/Ball.hpp
+++ src/Ball.hpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
+#include <vector>
 class Ball {
 public:
 	Ball();
@@ -12,7 +12,8 @@ public:
 	void update(const sf::RenderWindow& window);
 	sf::CircleShape shape{25.f};
 	sf::Vector2f velocity{20.f,20.f};
-	sf::RectangleShape trailShape{{0.f,10.f}};
+	// sf::RectangleShape trailShape{{0.f,10.f}};
+	std::vector<sf::RectangleShape*> trails;
 private:
 	const sf::Vector2f& getPosition();
 	void setPosition(const sf::Vector2f& position);
@@ -20,15 +21,18 @@ private:
 
 Ball::Ball() {
 	this->shape.setFillColor(sf::Color::White);
-	this->trailShape.setOrigin(0, this->trailShape.getSize().y/2);
-	std::cout << "ball spawned\n";
+	// this->trailShape.setOrigin(0, this->trailShape.getSize().y/2);
 }
 /**
  * @brief yaa
  * @param window 
 */
 void Ball::render(sf::RenderWindow& window) {
-	window.draw(this->trailShape);
+	for (const sf::RectangleShape* trail : this->trails) {
+		window.draw(*trail);
+		delete trail;
+	}
+	this->trails.clear();
 	window.draw(this->shape);
 }
 void Ball::update(const sf::RenderWindow& window) {
@@ -63,9 +67,19 @@ void Ball::update(const sf::RenderWindow& window) {
 	const sf::Vector2f deltaPosition = position - previousPosition;
 	const float length = sqrt(deltaPosition.x * deltaPosition.x + deltaPosition.y * deltaPosition.y);
 	const float rotation = (atan2(deltaPosition.y, deltaPosition.x) * 180) / M_PI;
-	this->trailShape.setSize({length,10.0f});
-	this->trailShape.setPosition(previousPosition.x + this->shape.getRadius(), previousPosition.y + this->shape.getRadius());
-	this->trailShape.setRotation(rotation);
+	// this->trailShape.setSize({length,10.0f});
+	// this->trailShape.setPosition(previousPosition.x + this->shape.getRadius(), previousPosition.y + this->shape.getRadius());
+	// this->trailShape.setRotation(rotation);
+
+
+	sf::RectangleShape* trail = new sf::RectangleShape({length,10.0f});
+	trail->setOrigin(0, trail->getSize().y/2);
+	trail->setPosition(previousPosition.x + this->shape.getRadius(), previousPosition.y + this->shape.getRadius());
+	trail->setRotation(rotation);
+	this->trails.push_back(trail);
+
+
+
 }
 inline const sf::Vector2f& Ball::getPosition() {
 	return this->shape.getPosition();
